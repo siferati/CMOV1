aa7a0f5b7e32c15ae2eb77648977a83432395b42
tirafesi <tirafesi@hotmail.com>
2018-10-11 16:08:30 +0100

    initial link between db and server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,36 @@
 const express = require('express')
+const sqlite3 = require('sqlite3').verbose()
+
 const app = express()
 const port = 8080
+const db = new sqlite3.Database('db/db.db')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+
+	db.serialize(function () {
+
+		db.each('SELECT * FROM Users', function (err, row) {
+			console.log(err)
+			console.log(row)
+		})
+	})
+	res.send('Hello World!')
+})
+
+function cleanup () {
+	console.log('closing db connection...')
+	db.close((error) => {
+		if (error == null) {
+			console.log("db closed.")
+		} else {
+			console.log("error closing db.")
+			console.log(error)
+		}
+		process.exit()		
+	});
+}
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ server/db/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Users (
+	id INTEGER PRIMARY KEY,
+	name TEXT  NOT NULL
+);
+
+INSERT INTO Users (id, name) VALUES (1, "teste");
