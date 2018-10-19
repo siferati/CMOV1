f5e2a549314433e1e52da97bc431fc4dda35355d
tirafesi <tirafesi@hotmail.com>
2018-10-19 18:38:43 +0100

    started working on the restful services

--- server/app.js
+++ server/app.js
@@ -1,30 +1,59 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose()
 
 const app = express()
-const port = 8080
 const db = new sqlite3.Database('db/db.sqlite3')
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
 
 	db.serialize(function () {
 
 		db.each('SELECT * FROM Users', function (err, row) {
-			console.log(err)
 			console.log(row)
 		})
 	})
 	res.send('Hello World!')
 })
 
-function cleanup () {
-	console.log('closing db connection...')
-	db.close((error) => {
-		if (error == null) {
-			console.log("db closed.")
+app.get('/users/:id', (req, res) => {
+	const id = req.params.id
+	sql = `
+		SELECT *
+		FROM Users
+		WHERE id = ?;`
+	db.get(sql, id, (err, row) => {
+		if (err) {
+			console.log(err)
+			return res.status(500).send("Internal Server Error.");
 		} else {
-			console.log("error closing db.")
-			console.log(error)
+			return res.send(row);
+		}
+	})
+});
+
+app.post('/users', (req, res) => {
+	const { firstName, lastName, phone } = req.body
+	// TODO: create user
+});
+
+app.put('/users/:id', (req, res) => {
+	const id = parseInt(req.params.id)
+	const { firstName, lastName, phone } = req.body
+	// TODO: find and update user by id
+});
+
+
+
+function cleanup () {
+	console.log('Closing connection to database...')
+	db.close((error) => {
+		if (error !== null) {
+			console.log("Failed to close the connection - forced exit.")
+		} else {
+			console.log("Connection closed.")
 		}
 		process.exit()		
 	});
@@ -33,4 +62,6 @@ function cleanup () {
 process.on('SIGINT', cleanup);
 process.on('SIGTERM', cleanup);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(8080, () => {
+	console.log(`Server running on port 8080.`)
+})
